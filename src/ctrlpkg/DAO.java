cd827cba4840de1298c2269fd7e1f091a16ca188
Snesnopic <snesnopic@gmail.com>
2022-01-10 16:23:46 +0100

    Update DAO.java

--- src/ctrlpkg/DAO.java
+++ src/ctrlpkg/DAO.java
@@ -1,10 +1,13 @@
 package ctrlpkg;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface DAO<T> {
-	List<T> getAll();
-	T get(int index);
-	void update(T obj);
-	void delete(T obj);
+	List<T> getAll(String sql) throws SQLException;
+	T get(String sql) throws SQLException;
+	void update(T obj,String sql);
+	void delete(T obj,String sql);
+	void insert(T obj,String sql);
+	
 }
