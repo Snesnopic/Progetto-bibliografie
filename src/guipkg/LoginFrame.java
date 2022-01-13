1f9746c7c321b6986b290347f7f2aa85a6689298
Snesnopic <snesnopic@gmail.com>
2022-01-13 11:35:57 +0100

    Cambiato stile e colori, aggiunto logo Unina e logo finestra

--- src/guipkg/LoginFrame.java
+++ src/guipkg/LoginFrame.java
@@ -2,8 +2,10 @@ package guipkg;
 import ctrlpkg.*;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.imageio.ImageIO;
 import javax.swing.GroupLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -16,16 +18,17 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 
 @SuppressWarnings("serial")
 public class LoginFrame extends JFrame {
 	private JPanel loginWindow;
 	private JTextField CFField;
-	
 	private Controller c;
 	
-	public LoginFrame(Controller c){
-		setResizable(false);
+	public LoginFrame(Controller c) throws IOException
+	{
+        setIconImage(ImageIO.read(getClass().getResource("/logo.png")));
 		this.setC(c);
 		setBackground(Color.WHITE);
 		setTitle("Login");
@@ -33,9 +36,13 @@ public class LoginFrame extends JFrame {
 		setBounds(100, 100, 640, 480);
 				
 		loginWindow = new JPanel();
+		loginWindow.setBackground(new Color(23, 33, 43));
 		setContentPane(loginWindow);	
 		
 		CFField = new JTextField();
+		CFField.setForeground(new Color(255, 255, 255));
+		CFField.setHorizontalAlignment(SwingConstants.CENTER);
+		CFField.setBackground(new Color(14, 22, 33));
 		CFField.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) 
@@ -47,77 +54,106 @@ public class LoginFrame extends JFrame {
 							JOptionPane.showMessageDialog(null, "Errore di input");		
 						else
 						{
-							if(c.login(CFField.getText()))
-								JOptionPane.showMessageDialog(null, "Login riuscito");
-							else
-								JOptionPane.showMessageDialog(null, "Login fallito");
+							CFField.setText(CFField.getText().toUpperCase());
+							try
+							{
+								if(c.login(CFField.getText()))
+									JOptionPane.showMessageDialog(null, "Login riuscito");
+								else
+									JOptionPane.showMessageDialog(null, "Login fallito");
+							}
+							catch (HeadlessException | IOException e1)
+							{
+								JOptionPane.showMessageDialog(null,"Errore: "+e1.getMessage());
+							}
 						}
 				}
 			}
 		});
-		CFField.setFont(new Font("Yu Gothic UI Light", Font.PLAIN, 11));
+		CFField.setFont(new Font("Yu Gothic UI", Font.PLAIN, 16));
 		CFField.setToolTipText("Username");
 		
 		
 		
 		JLabel loginLabel = new JLabel("Effettua l'accesso");
+		loginLabel.setBackground(new Color(255, 255, 255));
+		loginLabel.setForeground(new Color(255, 255, 255));
 		loginLabel.setVerticalAlignment(SwingConstants.TOP);
 		loginLabel.setHorizontalAlignment(SwingConstants.CENTER);
-		loginLabel.setFont(new Font("Yu Gothic UI", Font.PLAIN, 41));
+		loginLabel.setFont(new Font("Yu Gothic UI", Font.BOLD, 41));
 		
 		JLabel CFLabel = new JLabel("Codice Fiscale");
-		CFLabel.setFont(new Font("Yu Gothic UI", Font.PLAIN, 12));
+		CFLabel.setBackground(new Color(255, 255, 255));
+		CFLabel.setForeground(new Color(255, 255, 255));
+		CFLabel.setFont(new Font("Yu Gothic UI", Font.PLAIN, 18));
 		
 		JButton loginButton = new JButton("OK");
-		loginButton.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		loginButton.setForeground(new Color(255, 255, 255));
+		loginButton.setFont(new Font("Yu Gothic UI", Font.PLAIN, 16));
 		loginButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) 
 			{
 				if(CFField.getText().isEmpty())
-					JOptionPane.showMessageDialog(null, "Errore di input");		
+					JOptionPane.showMessageDialog(null, "Errore di input");
 				else
 				{
-					if(c.login(CFField.getText()))
-						JOptionPane.showMessageDialog(null, "Login riuscito");
-					else
-						JOptionPane.showMessageDialog(null, "Login fallito");
+					CFField.setText(CFField.getText().toUpperCase());
+					try
+					{
+						if(c.login(CFField.getText()))
+							JOptionPane.showMessageDialog(null, "Login riuscito");
+						else
+							JOptionPane.showMessageDialog(null, "Login fallito");
+					}
+					catch (HeadlessException | IOException e1)
+					{
+						JOptionPane.showMessageDialog(null,"Errore: "+e1.getMessage());
+					}
 				}
 			}
 		});
-		loginButton.setBackground(Color.WHITE);
+		loginButton.setBackground(new Color(14, 22, 33));
+		
+		JLabel uninaLogoLabel = new JLabel("");
+		uninaLogoLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		GroupLayout gl_loginWindow = new GroupLayout(loginWindow);
 		gl_loginWindow.setHorizontalGroup(
-			gl_loginWindow.createParallelGroup(Alignment.LEADING)
+			gl_loginWindow.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_loginWindow.createSequentialGroup()
 					.addGap(100)
 					.addGroup(gl_loginWindow.createParallelGroup(Alignment.LEADING)
-						.addComponent(loginButton, Alignment.TRAILING, GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE)
 						.addGroup(gl_loginWindow.createSequentialGroup()
-							.addComponent(CFLabel)
-							.addPreferredGap(ComponentPlacement.RELATED, 350, Short.MAX_VALUE))
-						.addComponent(CFField, GroupLayout.DEFAULT_SIZE, 424, Short.MAX_VALUE))
-					.addGap(100))
+							.addComponent(CFLabel, GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+							.addGap(414))
+						.addGroup(gl_loginWindow.createSequentialGroup()
+							.addGroup(gl_loginWindow.createParallelGroup(Alignment.TRAILING)
+								.addComponent(loginButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+								.addComponent(CFField, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE))
+							.addGap(124))))
 				.addGroup(gl_loginWindow.createSequentialGroup()
-					.addGap(93)
-					.addComponent(loginLabel, GroupLayout.PREFERRED_SIZE, 439, Short.MAX_VALUE)
-					.addGap(92))
+					.addGroup(gl_loginWindow.createParallelGroup(Alignment.TRAILING)
+						.addComponent(uninaLogoLabel, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE)
+						.addComponent(loginLabel, GroupLayout.DEFAULT_SIZE, 624, Short.MAX_VALUE))
+					.addGap(0))
 		);
 		gl_loginWindow.setVerticalGroup(
-			gl_loginWindow.createParallelGroup(Alignment.TRAILING)
+			gl_loginWindow.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_loginWindow.createSequentialGroup()
-					.addContainerGap(20, Short.MAX_VALUE)
-					.addComponent(loginLabel)
+					.addComponent(uninaLogoLabel, GroupLayout.PREFERRED_SIZE, 155, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(loginLabel, GroupLayout.PREFERRED_SIZE, 56, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(CFLabel)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(CFField, GroupLayout.PREFERRED_SIZE, 20, GroupLayout.PREFERRED_SIZE)
+					.addComponent(CFField, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.UNRELATED)
-					.addComponent(loginButton, GroupLayout.PREFERRED_SIZE, 33, GroupLayout.PREFERRED_SIZE)
-					.addGap(286))
+					.addComponent(loginButton, GroupLayout.PREFERRED_SIZE, 35, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(123, Short.MAX_VALUE))
 		);
 		loginWindow.setLayout(gl_loginWindow);
+		uninaLogoLabel.setIcon(new StretchIcon(getClass().getClassLoader().getResource("unina_logo.png")));
 	}
-	void emptyFields()
+	public void emptyFields()
 	{
 		CFField.setText("");
 	}
