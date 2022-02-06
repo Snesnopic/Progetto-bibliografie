34f032a4a6e528d1ea82fb9675bb6bb82aa64dab
Snesnopic <snesnopic@gmail.com>
2022-02-06 15:09:07 +0100

    Aggiunta classe per pannello vista riferimenti

---
+++ src/guipkg/ViewRifPanel.java
@@ -0,0 +1,32 @@
+package guipkg;
+
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class ViewRifPanel extends JPanel
+{
+
+	public ViewRifPanel()
+	{
+		setBackground(new Color(23, 33, 43));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0};
+		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{1.0, 0.0, 1.0, Double.MIN_VALUE};
+		setLayout(gridBagLayout);
+		
+		JLabel lblNewLabel = new JLabel("New label");
+		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNewLabel.gridx = 1;
+		gbc_lblNewLabel.gridy = 1;
+		add(lblNewLabel, gbc_lblNewLabel);
+	}
+
+}
