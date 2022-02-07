e8bfe3ef545969cb40ee1584ddaa01a925696ef4
Snesnopic <snesnopic@gmail.com>
2022-02-07 09:02:20 +0100

    Pulizia codice

--- src/guipkg/ViewRifPanel.java
+++ src/guipkg/ViewRifPanel.java
@@ -4,26 +4,25 @@ import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
-
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.Font;
 import javax.swing.JList;
-import javax.swing.AbstractListModel;
 import javax.swing.ListSelectionModel;
 
 public class ViewRifPanel extends JPanel
 {
 	private JButton backButton;
-	public ViewRifPanel()
+	private JList<String> myRifList;
+	public ViewRifPanel(String[] riferimenti)
 	{
 		setBackground(new Color(23, 33, 43));
 		GridBagLayout gridBagLayout = new GridBagLayout();
 		gridBagLayout.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.rowHeights = new int[]{100, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 1.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowHeights = new int[]{100, 0, 0, 0, 0, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, 0.0, 0.0, 1.0, 0.0, 1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
 		setBackButton(new JButton("Indietro"));
@@ -48,44 +47,35 @@ public class ViewRifPanel extends JPanel
 		selectRifLabel.setForeground(Color.WHITE);
 		selectRifLabel.setFont(new Font("Yu Gothic UI", Font.PLAIN, 16));
 		GridBagConstraints gbc_selectRifLabel = new GridBagConstraints();
-		gbc_selectRifLabel.anchor = GridBagConstraints.SOUTH;
+		gbc_selectRifLabel.fill = GridBagConstraints.VERTICAL;
 		gbc_selectRifLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_selectRifLabel.gridx = 2;
 		gbc_selectRifLabel.gridy = 1;
 		add(selectRifLabel, gbc_selectRifLabel);
 		
-		JList list = new JList();
-		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		list.setModel(new AbstractListModel() {
-			String[] values = new String[] {"1", "2", "3", "4", "5"};
-			public int getSize() {
-				return values.length;
-			}
-			public Object getElementAt(int index) {
-				return values[index];
-			}
-		});
-		list.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
-		GridBagConstraints gbc_list = new GridBagConstraints();
-		gbc_list.anchor = GridBagConstraints.SOUTH;
-		gbc_list.gridheight = 2;
-		gbc_list.insets = new Insets(0, 0, 5, 5);
-		gbc_list.fill = GridBagConstraints.HORIZONTAL;
-		gbc_list.gridx = 3;
-		gbc_list.gridy = 1;
-		add(list, gbc_list);
+		setMyRifList(new JList<String>());
+		myRifList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		myRifList.setListData(riferimenti);
+		myRifList.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
+		GridBagConstraints gbc_myRifList = new GridBagConstraints();
+		gbc_myRifList.gridheight = 2;
+		gbc_myRifList.insets = new Insets(0, 0, 5, 5);
+		gbc_myRifList.fill = GridBagConstraints.BOTH;
+		gbc_myRifList.gridx = 3;
+		gbc_myRifList.gridy = 1;
+		add(myRifList, gbc_myRifList);
 		
 		JButton deleteButton = new JButton("Cancella");
 		deleteButton.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
 		GridBagConstraints gbc_deleteButton = new GridBagConstraints();
-		gbc_deleteButton.anchor = GridBagConstraints.SOUTH;
-		gbc_deleteButton.fill = GridBagConstraints.HORIZONTAL;
+		gbc_deleteButton.fill = GridBagConstraints.BOTH;
 		gbc_deleteButton.insets = new Insets(0, 0, 5, 5);
 		gbc_deleteButton.gridx = 4;
 		gbc_deleteButton.gridy = 1;
 		add(deleteButton, gbc_deleteButton);
 		
 		JButton editButton = new JButton("Modifica");
+		editButton.setFont(new Font("Yu Gothic UI", Font.PLAIN, 11));
 		GridBagConstraints gbc_editButton = new GridBagConstraints();
 		gbc_editButton.anchor = GridBagConstraints.NORTH;
 		gbc_editButton.fill = GridBagConstraints.HORIZONTAL;
@@ -96,7 +86,7 @@ public class ViewRifPanel extends JPanel
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.insets = new Insets(0, 0, 0, 5);
+		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
 		gbc_lblNewLabel.gridx = 3;
 		gbc_lblNewLabel.gridy = 3;
 		add(lblNewLabel, gbc_lblNewLabel);
@@ -107,6 +97,12 @@ public class ViewRifPanel extends JPanel
 	public void setBackButton(JButton backButton) {
 		this.backButton = backButton;
 		backButton.setForeground(Color.WHITE);
+	}
+	public JList<String> getMyRifList() {
+		return myRifList;
+	}
+	public void setMyRifList(JList<String> myRifList) {
+		this.myRifList = myRifList;
 	}
 
 }
