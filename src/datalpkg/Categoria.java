9d54e45d07d1d6657a9a5d2ee8f5937f04cd14d2
Snesnopic <snesnopic@gmail.com>
2022-02-07 08:59:05 +0100

    Aggiornata classe dati per compatibilità col nuovo database

--- src/datalpkg/Categoria.java
+++ src/datalpkg/Categoria.java
@@ -1,10 +1,10 @@
 package datalpkg;
 
 public class Categoria {
+	private int id_Cat;
 	private String nome;
-	private String descrizione;
-	private String autore;
-	private Categoria generaliz;
+	private int autore;
+	private int id_Generaliz;
 	
 	public String getNome() {
 		return nome;
@@ -12,36 +12,40 @@ public class Categoria {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-	public String getDescrizione() {
-		return descrizione;
-	}
-	public void setDescrizione(String descrizione) {
-		this.descrizione = descrizione;
-	}
-	public String getAutore() {
+
+
+	public int getAutore() {
 		return autore;
 	}
-	public void setAutore(String autore) {
+	public void setAutore(int autore) {
 		this.autore = autore;
 	}
-	public Categoria getGeneraliz() {
-		return generaliz;
+	public int getGeneraliz() {
+		return id_Generaliz;
 	}
-	public void setGeneraliz(Categoria generaliz) {
-		this.generaliz = generaliz;
+	public void setGeneraliz(int generaliz) {
+		this.id_Generaliz = generaliz;
 	}
-	public Categoria(String nome, String descrizione, String autore, Categoria generaliz) {
+	
+	public Categoria(int id_Cat,String nome, int autore, int generaliz) {
 		super();
+		this.id_Cat = id_Cat;
 		this.nome = nome;
-		this.descrizione = descrizione;
 		this.autore = autore;
-		this.generaliz = generaliz;
+		this.id_Generaliz = generaliz;
 	}
-	public Categoria(String nome, String descrizione, String autore)
+	public Categoria(int id_Cat,String nome, int autore)
 	{
 		super();
+		this.id_Cat = id_Cat;
 		this.nome = nome;
-		this.descrizione = descrizione;
 		this.autore = autore;
+		this.id_Generaliz = 0;
+	}
+	public int getId_Cat() {
+		return id_Cat;
+	}
+	public void setId_Cat(int id_Cat) {
+		this.id_Cat = id_Cat;
 	}
 }
