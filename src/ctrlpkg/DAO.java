9bfc2dec05f99d6293ef9423868c979e676a0c73
Snesnopic <snesnopic@gmail.com>
2022-02-07 08:57:45 +0100

    Le operazioni CRUD ora richiedono solo un istanza dell'oggetto, non anche una stringa SQL

--- src/ctrlpkg/DAO.java
+++ src/ctrlpkg/DAO.java
@@ -6,8 +6,8 @@ import java.util.List;
 public interface DAO<T> {
 	List<T> getAll(String sql) throws SQLException;
 	T get(String sql) throws SQLException;
-	void update(T obj,String sql);
-	void delete(T obj,String sql);
-	void insert(T obj,String sql);
+	void update(T obj) throws SQLException;
+	void delete(T obj) throws SQLException;
+	void insert(T obj) throws SQLException;
 	
 }
