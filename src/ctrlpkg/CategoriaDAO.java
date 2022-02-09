ce1c6d03f15943b84f4dfafa067511db2175dee0
Snesnopic <snesnopic@gmail.com>
2022-02-09 17:48:54 +0100

    Rimossi metodi delete

--- src/ctrlpkg/CategoriaDAO.java
+++ src/ctrlpkg/CategoriaDAO.java
@@ -6,64 +6,50 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 public class CategoriaDAO implements DAO<Categoria> {
-    private final DBConnection dbc = DBConnection.getInstance();
+	private final DBConnection dbc = DBConnection.getInstance();
 
-    @Override
-    public List<Categoria> getAll(String sql) throws SQLException {
-        ResultSet rs = dbc.executeQuery(sql);
-        List<Categoria> list = new ArrayList<>();
-        while (rs.next()) {
-            list.add(extract(rs));
-        }
-        return list;
-    }
+	@Override
+	public List<Categoria> getAll(String sql) throws SQLException {
+		ResultSet rs = dbc.executeQuery(sql);
+		List<Categoria> list = new ArrayList<>();
+		while (rs.next()) {
+			list.add(extract(rs));
+		}
+		return list;
+	}
 
-    @Override
-    public Categoria get(String sql) throws SQLException {
-        ResultSet rs = dbc.executeQuery(sql);
-        if (rs.next())
-            return extract(rs);
-        else
-            return null;
-    }
+	@Override
+	public Categoria get(String sql) throws SQLException {
+		ResultSet rs = dbc.executeQuery(sql);
+		if (rs.next())
+			return extract(rs);
+		else
+			return null;
+	}
 
-    @Override
-    public void update(Categoria obj) throws SQLException {
-        String sql = "UPDATE categoria SET descr_categoria = '" + obj.getNome() + "',id_super_categoria = " + obj.getGeneraliz() + ",id_utente = " + obj.getAutore() + " WHERE id_categoria = " + obj.getId_Cat();
-        dbc.execute(sql);
-    }
+	@Override
+	public void update(Categoria obj) throws SQLException {
+		String sql = "UPDATE categoria SET descr_categoria = '" + obj.getNome() + "',id_super_categoria = "
+				+ obj.getGeneraliz() + ",id_utente = " + obj.getAutore() + " WHERE id_categoria = " + obj.getId_Cat();
+		dbc.execute(sql);
+	}
 
-    @Override
-    public void delete(Categoria obj) throws SQLException {
-        String sql = "DELETE FROM categoria WHERE id_categoria = " + obj.getId_Cat();
+	@Override
+	public void insert(Categoria obj) throws SQLException {
+		String sql = "INSERT INTO categoria VALUES(" + obj.getId_Cat() + ",'" + obj.getNome() + "'";
+		if (obj.getGeneraliz() == 0)
+			sql = sql.concat(",null");
+		else {
+			sql = sql.concat("," + obj.getGeneraliz());
+		}
+		sql = sql.concat("," + obj.getAutore() + ")");
+		dbc.execute(sql);
+	}
 
-        dbc.execute(sql);
-    }
-
-    @Override
-    public void insert(Categoria obj) throws SQLException {
-        String sql = "INSERT INTO categoria VALUES(" + obj.getId_Cat() + ",'" + obj.getNome() + "'";
-        if (obj.getGeneraliz() == 0)
-            sql = sql.concat(",null");
-        else {
-            sql = sql.concat("," + obj.getGeneraliz());
-        }
-
-        sql = sql.concat("," + obj.getAutore() + ")");
-        dbc.execute(sql);
-    }
-
-    private Categoria extract(ResultSet rs) throws SQLException {
-        // TODO Auto-generated method stub
-        Categoria c = new Categoria(rs.getInt("id_categoria"), rs.getString("descr_categoria"), rs.getInt("id_utente"));
-        Integer generalizzazione = rs.getInt("id_super_categoria");
-        if (Objects.isNull(generalizzazione))
-            c.setGeneraliz(this.get("SELECT * FROM categoria WHERE nome='" + rs.getInt("id_super_categoria") + "'").getId_Cat());
-        return c;
-    }
-
-
+	private Categoria extract(ResultSet rs) throws SQLException {
+		return new Categoria(rs.getInt("id_categoria"), rs.getString("descr_categoria"), rs.getInt("id_utente"),
+				rs.getInt("id_super_categoria"));
+	}
 }
--- src/ctrlpkg/UtenteDAO.java
+++ src/ctrlpkg/UtenteDAO.java
@@ -10,59 +10,56 @@ import java.util.List;
 import java.util.Objects;
 
 public class UtenteDAO implements DAO<Utente> {
-    private final DBConnection dbc = DBConnection.getInstance();
+	private final DBConnection dbc = DBConnection.getInstance();
 
-    @Override
-    public List<Utente> getAll(String sql) throws SQLException {
-        ResultSet rs = dbc.executeQuery(sql);
-        List<Utente> list = new ArrayList<>();
-        while (rs.next()) {
-            list.add(extract(rs));
-        }
-        return list;
-    }
+	@Override
+	public List<Utente> getAll(String sql) throws SQLException {
+		ResultSet rs = dbc.executeQuery(sql);
+		List<Utente> list = new ArrayList<>();
+		while (rs.next()) {
+			list.add(extract(rs));
+		}
+		return list;
+	}
 
-    @Override
-    public Utente get(String sql) throws SQLException {
-        ResultSet rs = dbc.executeQuery(sql);
-        if (rs.next())
-            return extract(rs);
-        else
-            return null;
-    }
+	@Override
+	public Utente get(String sql) throws SQLException {
+		ResultSet rs = dbc.executeQuery(sql);
+		if (rs.next())
+			return extract(rs);
+		else
+			return null;
+	}
 
-    @Override
-    public void update(Utente obj) throws SQLException {
-        String sql = "UPDATE utente SET nome_utente = '" + obj.getNome() + "',cognome_utente = '" + obj.getCognome() + "',data_inizio_validita = '" + obj.getInizio().toString() + "',";
-        if (Objects.isNull(obj.getFine()))
-            sql = sql.concat("NULL");
-        else
-            sql = sql.concat("'" + obj.getFine().toString() + "' ");
-        sql = sql.concat("WHERE id_utente = " + obj.getUser_ID());
-        dbc.execute(sql);
-    }
+	@Override
+	public void update(Utente obj) throws SQLException {
+		String sql = "UPDATE utente SET nome_utente = '" + obj.getNome() + "',cognome_utente = '" + obj.getCognome()
+				+ "',data_inizio_validita = '" + obj.getInizio().toString() + "',";
+		if (Objects.isNull(obj.getFine()))
+			sql = sql.concat("NULL");
+		else
+			sql = sql.concat("'" + obj.getFine().toString() + "' ");
+		sql = sql.concat("WHERE id_utente = " + obj.getUser_ID());
+		dbc.execute(sql);
+	}
 
-    @Override
-    public void delete(Utente obj) throws SQLException {
-        String sql = "DELETE FROM utente WHERE id_utente = " + obj.getUser_ID();
-        dbc.execute(sql);
-    }
+	@Override
+	public void insert(Utente obj) throws SQLException {
+		// TODO Auto-generated method stub
+		String sql = "INSERT INTO utente VALUES(" + obj.getUser_ID() + ",'" + obj.getNome() + "','" + obj.getCognome()
+				+ "','" + new SimpleDateFormat("dd/MM/yyyy").format(obj.getInizio()) + "',";
+		if (Objects.isNull(obj.getFine()))
+			sql = sql.concat("NULL");
+		else
+			sql = sql.concat("'" + new SimpleDateFormat("dd/MM/yyyy").format(obj.getFine()) + "' ");
+		sql = sql.concat(")");
+		System.out.println(sql);
+		dbc.execute(sql);
+	}
 
-    @Override
-    public void insert(Utente obj) throws SQLException {
-        // TODO Auto-generated method stub
-        String sql = "INSERT INTO utente VALUES(" + obj.getUser_ID() + ",'" + obj.getNome() + "','" + obj.getCognome() + "','" + new SimpleDateFormat("dd/MM/yyyy").format(obj.getInizio()) + "',";
-        if (Objects.isNull(obj.getFine()))
-            sql = sql.concat("NULL");
-        else
-            sql = sql.concat("'" + new SimpleDateFormat("dd/MM/yyyy").format(obj.getFine()) + "' ");
-        sql = sql.concat(")");
-        System.out.println(sql);
-        dbc.execute(sql);
-    }
+	private Utente extract(ResultSet rs) throws SQLException {
 
-    private Utente extract(ResultSet rs) throws SQLException {
-
-        return new Utente(rs.getString("nome_utente"), rs.getString("cognome_utente"), rs.getInt("id_utente"), rs.getDate("data_inizio_validita"), rs.getDate("data_fine_validita"));
-    }
+		return new Utente(rs.getString("nome_utente"), rs.getString("cognome_utente"), rs.getInt("id_utente"),
+				rs.getDate("data_inizio_validita"), rs.getDate("data_fine_validita"));
+	}
 }
