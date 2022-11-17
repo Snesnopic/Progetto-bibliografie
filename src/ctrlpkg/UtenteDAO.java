501d1866734519bf008f5f769e4cdf4aa7002d66
Giuseppe <snesnopic@gmail.com>
2022-11-17 10:02:24 +0100

    Semplificato e modernizzato un po' di codice e aggiunto final dove possibile

--- src/ctrlpkg/UtenteDAO.java
+++ src/ctrlpkg/UtenteDAO.java
@@ -8,11 +8,12 @@ import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
 
 public class UtenteDAO implements DAO<Utente> {
 	private final DBConnection dbc = DBConnection.getInstance();
-	private static UtenteDAO instance = null;
+	private static UtenteDAO instance;
 	private UtenteDAO() {
 
 	}
@@ -24,8 +25,8 @@ public class UtenteDAO implements DAO<Utente> {
 
 	@Override
 	public List<Utente> getAll() throws SQLException {
-		ResultSet rs = dbc.executeQuery("SELECT * FROM utente");
-		List<Utente> list = new ArrayList<>();
+		final ResultSet rs = dbc.executeQuery("SELECT * FROM utente");
+		final List<Utente> list = new ArrayList<>();
 		while (rs.next()) {
 			list.add(extract(rs));
 		}
@@ -33,17 +34,17 @@ public class UtenteDAO implements DAO<Utente> {
 	}
 
 	@Override
-	public Utente get(int id) throws SQLException {
-		ResultSet rs = dbc.executeQuery("SELECT * FROM utente WHERE id_utente = " + id);
+	public Utente get(final int id) throws SQLException {
+		final ResultSet rs = dbc.executeQuery("SELECT * FROM utente WHERE id_utente = " + id);
 		if (rs.next())
 			return extract(rs);
 		else
 			return null;
 	}
 
-	public List<Utente> getByRif(Riferimento r) throws SQLException {
-		ResultSet rs = dbc.executeQuery("SELECT utente.* FROM utente NATURAL JOIN autore_riferimento WHERE id_riferimento = " + r.getId_Rif());
-		List<Utente> list = new ArrayList<>();
+	public List<Utente> getByRif(final Riferimento r) throws SQLException {
+		final ResultSet rs = dbc.executeQuery("SELECT utente.* FROM utente NATURAL JOIN autore_riferimento WHERE id_riferimento = " + r.getId_Rif());
+		final List<Utente> list = new ArrayList<>();
 		while (rs.next()) {
 			list.add(extract(rs));
 		}
@@ -52,38 +53,37 @@ public class UtenteDAO implements DAO<Utente> {
 
 	public int getNextId() throws SQLException
 	{
-		ResultSet rs = dbc.executeQuery("SELECT MAX(id_utente) FROM utente");
+		final ResultSet rs = dbc.executeQuery("SELECT MAX(id_utente) FROM utente");
 		rs.next();
 		return rs.getInt(1) + 1;
 	}
 
 	@Override
-	public void update(Utente obj) throws SQLException {
+	public void update(final Utente obj) throws SQLException {
 		String sql = "UPDATE utente SET nome_utente = '" + obj.getNome() + "',cognome_utente = '" + obj.getCognome()
 				+ "',data_inizio_validita = '" + obj.getInizio().toString() + "',";
 		if (Objects.isNull(obj.getFine()))
-			sql = sql.concat("NULL");
+			sql = sql + "NULL";
 		else
-			sql = sql.concat("'" + obj.getFine().toString() + "' ");
-		sql = sql.concat("WHERE id_utente = " + obj.getUser_ID());
+			sql = sql + "'" + obj.getFine().toString() + "' ";
+		sql = sql + "WHERE id_utente = " + obj.getUser_ID();
 		dbc.execute(sql);
 	}
 
 	@Override
-	public void insert(Utente obj) throws SQLException {
+	public void insert(final Utente obj) throws SQLException {
 		// TODO Auto-generated method stub
 		String sql = "INSERT INTO utente VALUES(" + obj.getUser_ID() + ",'" + obj.getNome() + "','" + obj.getCognome()
-				+ "','" + new SimpleDateFormat("dd/MM/yyyy").format(obj.getInizio()) + "',";
+				+ "','" + new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(obj.getInizio()) + "',";
 		if (Objects.isNull(obj.getFine()))
-			sql = sql.concat("NULL");
+			sql = sql + "NULL";
 		else
-			sql = sql.concat("'" + new SimpleDateFormat("dd/MM/yyyy").format(obj.getFine()) + "' ");
-		sql = sql.concat(")");
-		System.out.println(sql);
+			sql = sql + "'" + new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(obj.getFine()) + "' ";
+		sql = sql + ")";
 		dbc.execute(sql);
 	}
 
-	private Utente extract(ResultSet rs) throws SQLException {
+	private static Utente extract(final ResultSet rs) throws SQLException {
 
 		return new Utente(rs.getString("nome_utente"), rs.getString("cognome_utente"), rs.getInt("id_utente"),
 				rs.getDate("data_inizio_validita"), rs.getDate("data_fine_validita"));
